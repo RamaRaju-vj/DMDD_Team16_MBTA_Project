8353e23fc69e71d37247360b7d9444d92e453231
alshinikhil <alshi.ni@northeastern.edu>
2024-12-04 22:08:29 -0500

    changing the views

--- Views/Exec_Views/Exec_Customer_Summary_mbta_admin.sql
+++
@@ -1,4 +0,0 @@
-
--- exec as mbta admin to view Customer_Summary
-SELECT * FROM MBTA_ADMIN.Customer_Summary;
-
--- Views/Views_Creation_mbta_admin/Refunds_Cancellation_Summary_View.sql
+++ Views/Views_Creation_mbta_admin/Refunds_Cancellation_Summary_View.sql
@@ -1,17 +1,5 @@
 -- Create Refunds_Cancellation_Summary View
-DECLARE
-    v_count NUMBER;
-BEGIN
-    -- Check if the view exists
-    SELECT COUNT(*) INTO v_count FROM user_views WHERE view_name = 'REFUNDS_CANCELLATION_SUMMARY';
 
-    -- Drop the view if it exists
-    IF v_count > 0 THEN
-        EXECUTE IMMEDIATE 'DROP VIEW REFUNDS_CANCELLATION_SUMMARY';
-    END IF;
-
-    -- Recreate the view
-    EXECUTE IMMEDIATE '
     CREATE VIEW REFUNDS_CANCELLATION_SUMMARY AS
     SELECT 
         t.transaction_id_pk AS transaction_id,
@@ -22,7 +10,5 @@ BEGIN
     FROM 
         MBTA_ADMIN.transaction_table t
     WHERE 
-        t.refund_status = ''Refunded'' OR t.transaction_status = ''Cancelled''';
-    DBMS_OUTPUT.PUT_LINE('Refunds_Cancellation_Summary view created successfully.');
-END;
-/
+        t.refund_status = ''Refunded'' OR t.transaction_status = ''Cancelled'';
+  
